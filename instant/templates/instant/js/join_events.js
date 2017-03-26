bc4102c4314842f243ff98935ef2ed57f918047d
Win <synwe@yahoo.com>
2017-03-26 10:11:06 +0000

    Change js debug variable

--- instant/templates/instant/js/join_events.js
+++ instant/templates/instant/js/join_events.js
@@ -1,15 +1,15 @@
 "join": function(message) {
-	if ( debug === true ) {console.log('JOIN: '+JSON.stringify(message))};
+	if ( instantDebug === true ) {console.log('JOIN: '+JSON.stringify(message))};
 },
 "leave": function(message) {
-	if ( debug === true ) {console.log('LEAVE: '+JSON.stringify(message))};
+	if ( instantDebug === true ) {console.log('LEAVE: '+JSON.stringify(message))};
 },
 "subscribe": function(context) {
-	if ( debug === true ) {console.log('SUSCRIBE: '+JSON.stringify(context))};
+	if ( instantDebug === true ) {console.log('SUSCRIBE: '+JSON.stringify(context))};
 },
 "error": function(errContext) {
-	if ( debug === true ) {console.log('ERROR: '+JSON.stringify(errContext))};
+	if ( instantDebug === true ) {console.log('ERROR: '+JSON.stringify(errContext))};
 },
 "unsubscribe": function(context) {
-	if ( debug === true ) {console.log('UNSUSCRIBE: '+JSON.stringify(context))};
+	if ( instantDebug === true ) {console.log('UNSUSCRIBE: '+JSON.stringify(context))};
 }
