c109c6d665394825a9ee8f1d90e73f9efd341c36
Win <synwe@yahoo.com>
2017-07-30 16:05:33 +0000

    Lint pep8

--- instant/producers.py
+++ instant/producers.py
@@ -5,51 +5,58 @@ import json
 from cent import Client, CentException
 from instant.conf import SITE_NAME, CENTRIFUGO_HOST, CENTRIFUGO_PORT, SECRET_KEY, SITE_SLUG, PUBLIC_CHANNEL, BROADCAST_WITH
 if BROADCAST_WITH == "go":
-    import os, instant
+    import os
+    import instant
 
 
 def _get_channel(channel, target):
     if channel is None:
         if target is not None:
             if target == "superuser":
-                channel = "$"+SITE_SLUG+'_admin'
+                channel = "$" + SITE_SLUG + '_admin'
             elif target == "staff":
-                channel = "$"+SITE_SLUG+'_staff'
+                channel = "$" + SITE_SLUG + '_staff'
             elif target == "users":
-                channel = "$"+SITE_SLUG+'_users'
+                channel = "$" + SITE_SLUG + '_users'
             else:
                 return False, None
         else:
             channel = PUBLIC_CHANNEL
     return channel
 
+
 def publish_py(message, event_class="default", data={}, channel=None, site=SITE_NAME, target=None):
-    cent_url = CENTRIFUGO_HOST+":"+str(CENTRIFUGO_PORT)
+    cent_url = CENTRIFUGO_HOST + ":" + str(CENTRIFUGO_PORT)
     client = Client(cent_url, SECRET_KEY, timeout=1)
     channel = _get_channel(channel, target)
-    payload = {"message": message, "channel":channel, 'event_class':event_class, "data":data , "site":site}
+    payload = {"message": message, "channel": channel,
+               'event_class': event_class, "data": data, "site": site}
     err = None
     try:
         client.publish(channel, payload)
     except CentException as e:
         err = str(e)
     if event_class.lower() == "debug":
-        print ("[DEBUG] ", str(json.dumps(payload)))
+        print("[DEBUG] ", str(json.dumps(payload)))
     return err
+
 
 def publish_go(message, event_class="default", data={}, channel=None, site=SITE_NAME, target=None):
     channel = _get_channel(channel, target)
-    channel = channel.replace("$","-_-")
-    conn='-host="'+CENTRIFUGO_HOST+'" -port="'+str(CENTRIFUGO_PORT)+'" -key="'+SECRET_KEY+'"'
-    params = conn+' -channel="'+channel+'" -event_class="'+event_class+'" -message="'+message+'" -data=\''+json.dumps(data)+"'"
+    channel = channel.replace("$", "-_-")
+    conn = '-host="' + CENTRIFUGO_HOST + '" -port="' + \
+        str(CENTRIFUGO_PORT) + '" -key="' + SECRET_KEY + '"'
+    params = conn + ' -channel="' + channel + '" -event_class="' + event_class + \
+        '" -message="' + message + '" -data=\'' + json.dumps(data) + "'"
     pth = os.path.dirname(instant.__file__)
-    gocmd=pth+'/go/publish '+params
+    gocmd = pth + '/go/publish ' + params
     os.system(gocmd)
     if event_class.lower() == "debug":
-        print ("[DEBUG] ", message, str(json.dumps(data)))
+        print("[DEBUG] ", message, str(json.dumps(data)))
     return
-    
+
+
 if BROADCAST_WITH == "go":
-    publish  = publish_go
+    publish = publish_go
 else:
     publish = publish_py
