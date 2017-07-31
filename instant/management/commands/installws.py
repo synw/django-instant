764f131a17e918d263919b9c42ba7044a0bec9b3
Win <synwe@yahoo.com>
2017-07-31 16:15:14 +0000

    Remove bash scripts in favor of pure python installer

--- instant/management/commands/installws.py
+++ instant/management/commands/installws.py
@@ -1,18 +1,53 @@
 from __future__ import print_function
-from django.core.management.base import BaseCommand
 import os
 import subprocess
+import json
 from django.conf import settings
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
     help = 'Install the Centrifugo websockets server for Linux'
 
     def handle(self, *args, **options):
+        centrifugo_version = "1.7.3"
+        fetch_url = "https://github.com/centrifugal/centrifugo/releases/download/v" + \
+            centrifugo_version + "/centrifugo-" + centrifugo_version + "-linux-386.zip"
+        subprocess.call(["wget", fetch_url])
+        dirname = "centrifugo-" + centrifugo_version + "-linux-386"
+        subprocess.call(["unzip", dirname + ".zip"])
+        subprocess.call(["mv", dirname, "centrifugo"])
+        subprocess.call(["rm", "-f", dirname + ".zip"])
+        subprocess.call(["centrifugo/centrifugo", "genconfig"])
+        subprocess.call(["mv", "config.json", "centrifugo"])
+        # generate settings
+        print("Updating settings")
         basepath = os.getcwd()
-        path = os.path.abspath(__file__)
-        modpath = os.path.dirname(path)
         project_name = settings.BASE_DIR.split("/")[-1:][0]
-        script = modpath + "/install.sh"
-        subprocess.call([script, project_name, basepath, modpath])
+        filepath = basepath + '/centrifugo/config.json'
+        filepathtmp = basepath + '/tmp.py'
+        with open(filepath) as f:
+            content = f.read()
+        f = open(filepath, "r")
+        f2 = open(filepathtmp, "w+")
+        conf = json.loads(content)
+        conf["anonymous"] = "true"
+        key = conf["secret"]
+        res = json.dumps(conf, indent=4).replace('"true"', 'true')
+        f2.write(res)
+        f2.close()
+        os.remove(filepath)
+        os.rename(filepathtmp, filepath)
+        # settings.py
+        extralines = 'SITE_SLUG = "' + project_name + '"\n'
+        extralines = extralines + 'SITE_NAME = SITE_SLUG\n'
+        extralines = extralines + 'CENTRIFUGO_SECRET_KEY = "' + key + '"\n'
+        extralines = extralines + 'CENTRIFUGO_HOST = "http://localhost"\n'
+        extralines = extralines + "CENTRIFUGO_PORT = 8001\n"
+        project_dir = basepath + "/" + project_name
+        filepath = project_dir + '/settings.py'
+        f = open(filepath, "a")
+        f.write("\n" + extralines + "\n")
+        f.close()
+        print("The Centrifugo websockets server is installed. Run it with python3 manage.py runws")
         return
--- setup.py
+++ setup.py
@@ -4,23 +4,23 @@ from setuptools import setup, find_packages
 version = __import__('instant').__version__
 
 setup(
-    name = 'django-instant',
+    name='django-instant',
     packages=find_packages(),
     include_package_data=True,
-    version = version,
-    description = 'Websockets for Django with Centrifugo ',
-    author = 'synw',
-    author_email = 'synwe@yahoo.com',
-    url = 'https://github.com/synw/django-instant', 
-    download_url = 'https://github.com/synw/django-instant/releases/tag/'+version, 
-    keywords = ['django', 'websockets', 'centrifugo'], 
-    classifiers = [
-          'Development Status :: 4 - Beta',
-          'Framework :: Django :: 1.11',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3.5',
-      ],
+    version=version,
+    description='Websockets for Django with Centrifugo ',
+    author='synw',
+    author_email='synwe@yahoo.com',
+    url='https://github.com/synw/django-instant',
+    download_url='https://github.com/synw/django-instant/releases/tag/' + version,
+    keywords=['django', 'websockets', 'centrifugo'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django :: 1.11',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ],
     install_requires=[
         'cent',
     ],
