ad3739998f29eceec2544189b1fdd2d0e4e592a9
Win <synwe@yahoo.com>
2017-08-12 10:37:22 +0000

    Add a control to enforce declarated channels privacy at startup

--- instant/apps.py
+++ instant/apps.py
@@ -1,7 +1,14 @@
 from __future__ import unicode_literals
-
 from django.apps import AppConfig
 
 
 class InstantConfig(AppConfig):
     name = 'instant'
+
+    def ready(self):
+        from instant.utils import _ensure_channel_is_private
+        from instant.conf import USERS_CHANNELS, STAFF_CHANNELS, SUPERUSER_CHANNELS
+        # ensure that the private channels are really private
+        private_chans = USERS_CHANNELS + STAFF_CHANNELS + SUPERUSER_CHANNELS
+        for chan in private_chans:
+            _ensure_channel_is_private(chan)
--- instant/utils.py
+++ instant/utils.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-
+from django.core.exceptions import ImproperlyConfigured
 from cent.core import generate_channel_sign
 from instant.conf import PUBLIC_CHANNEL, SECRET_KEY
 
@@ -11,6 +10,13 @@ def signed_response(channel, client):
 
 def _get_public_channel():
     return PUBLIC_CHANNEL
+
+
+def _ensure_channel_is_private(chanconf):
+    chan = chanconf[0]
+    if chan.startswith("$") is False:
+        msg = "Channel " + chan + " must start with a $ to be considered private"
+        raise ImproperlyConfigured(msg)
 
 
 def _get_chans_from_conf(chanconf, path=None):
