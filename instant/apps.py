3e4e375b276d617917b59f87bf502b31a0f5f54e
Win <synwe@yahoo.com>
2018-01-23 14:50:04 +0000

    Add deprecation warnings for some old settings

--- instant/apps.py
+++ instant/apps.py
@@ -18,6 +18,23 @@ class InstantConfig(AppConfig):
         global HANDLERS, CHANNELS, CHANNELS_NAMES
         from django.conf import settings
         from .utils import get_channels_for_roles
+        from .conf import ENABLE_STAFF_CHANNEL, ENABLE_USERS_CHANNEL, \
+            ENABLE_SUPERUSER_CHANNEL
+        col = '\033[91m'
+        endcol = '\033[0m'
+        if ENABLE_STAFF_CHANNEL is True:
+            print(col + "Warning from Django Instant" + endcol + ": the setting ENABLE_STAFF_CHANNEL "
+                  "will be deprecated in version 0.8. "
+                  "Please use declarative channels instead")
+        if ENABLE_USERS_CHANNEL is True:
+            print(col + "Warning from Django Instant" + endcol + ": the setting ENABLE_USERS_CHANNEL "
+                  "will be deprecated in version 0.8. "
+                  "Please use declarative channels instead")
+        if ENABLE_SUPERUSER_CHANNEL is True:
+            print(col + "Warning from Django Instant" + endcol + ": the setting ENABLE_SUPERUSER_CHANNEL "
+                  "will be deprecated in version 0.8. "
+                  "Please use declarative channels instead")
+
         CHANNELS, CHANNELS_NAMES = get_channels_for_roles()
         try:
             if settings.INSTANT_DEBUG is True:
