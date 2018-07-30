5e7be06d39d699927c27cf60e7354b08bec4162c
Win <synwe@yahoo.com>
2018-07-30 13:01:37 +0000

    Minor correction in conf

--- instant/conf.py
+++ instant/conf.py
@@ -24,11 +24,9 @@ DEFAULT_STAFF_CHANNEL = "$" + SITE_SLUG + "_staff"
 DEFAULT_SUPERUSER_CHANNEL = "$" + SITE_SLUG + "_admin"
 
 PUBLIC_CHANNELS = getattr(settings, 'INSTANT_PUBLIC_CHANNELS', [])
-USERS_CHANNELS = getattr(
-    settings, 'INSTANT_USERS_CHANNELS', [])
+USERS_CHANNELS = getattr(settings, 'INSTANT_USERS_CHANNELS', [])
 STAFF_CHANNELS = getattr(settings, 'INSTANT_STAFF_CHANNELS', [])
 SUPERUSER_CHANNELS = getattr(settings, 'INSTANT_SUPERUSER_CHANNELS', [])
-USERS_CHANNELS = getattr(settings, 'INSTANT_USERS_CHANNELS', [])
 
 
 EXCLUDE = getattr(settings, 'INSTANT_EXCLUDE', ["__presence__"])
