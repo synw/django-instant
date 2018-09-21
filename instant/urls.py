4e2df34b74f3ba1a1e5b9fdfafec0b35ddbdba9f
Win <synwe@yahoo.com>
2018-09-21 16:49:04 +0000

    Remove old code

--- instant/urls.py
+++ instant/urls.py
@@ -1,16 +1,8 @@
 # -*- coding: utf-8 -*-
-from django import VERSION
-if VERSION >= (2, 0):
-    from django.urls import reverse_lazy
-else:
-    from django.core.urlresolvers import reverse_lazy
 from django.conf.urls import url
-from django.views.generic import RedirectView
 from instant.views import FrontendView, PostMsgView
 
 urlpatterns = [
-    url(r'^redirect-home/$', RedirectView.as_view(url=reverse_lazy('instant-broadcast')),
-        name='instant-message-broadcasted'),
     url(r'^post/$', PostMsgView.as_view(), name="instant-post-msg"),
-    url(r'^', FrontendView.as_view(), name="instant-broadcast")
+    url(r'^', FrontendView.as_view(), name="instant-frontend")
 ]
