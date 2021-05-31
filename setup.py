5f7517eb69aeec45962d3274858f13861e8630d6
synw <synwx@protonmail.com>
2021-05-31 14:30:36 +0200

    Improve setup

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup, find_packages
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 version = __import__('instant').__version__
 
@@ -8,6 +12,8 @@ setup(
     include_package_data=True,
     version=version,
     description='Websockets for Django with Centrifugo ',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='synw',
     author_email='synwe@yahoo.com',
     url='https://github.com/synw/django-instant',
@@ -15,14 +21,13 @@ setup(
     keywords=['django', 'websockets', 'centrifugo'],
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 3.0',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.7',
     ],
     install_requires=[
-        'cent',
-        'django-cors-headers',
+        'cent'
     ],
     zip_safe=False
 )
