1134d94f6563042ddc809e1f56919c910b6cc0d4
synw <synwx@protonmail.com>
2021-05-31 14:31:06 +0200

    Improve defaults in the publish function

--- instant/producers.py
+++ instant/producers.py
@@ -6,21 +6,23 @@ from .conf import SITE_NAME, CENTRIFUGO_HOST, CENTRIFUGO_PORT, CENTRIFUGO_API_KE
 
 
 def publish(
-    channel, message, event_class="default", data={}, bucket=None, site=SITE_NAME
+    channel, message=None, event_class=None, data=None, bucket=None, site=SITE_NAME
 ):
-
+    if message is None and data is None:
+        raise ValueError("Provide either a message or data argument")
     cent_url = CENTRIFUGO_HOST
     if CENTRIFUGO_PORT is not None:
         cent_url += ":" + str(CENTRIFUGO_PORT)
     client = Client(cent_url, api_key=CENTRIFUGO_API_KEY, timeout=1)
-    payload = {
-        "message": message,
-        "channel": channel,
-        "event_class": event_class,
-        "bucket": bucket,
-        "data": data,
-        "site": site,
-    }
+    payload = {"channel": channel, "site": site}
+    if message is not None:
+        payload["message"] = message
+    if data is not None:
+        payload["data"] = data
+    if event_class is not None:
+        payload["event_class"] = event_class
+    if bucket is not None:
+        payload["bucket"] = bucket
     err = None
     try:
         print(f"Publishing to {cent_url} {payload}")
