f0180b43d5934dc78eb9f97eb808eb140ee6c94f
synw <synwx@protonmail.com>
2021-08-03 11:19:12 +0200

    Minor formating improvement

--- instant/models.py
+++ instant/models.py
@@ -33,13 +33,15 @@ class Channel(models.Model):
         max_length=120,
         verbose_name=_("Name"),
         unique=True,
-        help_text=_("Use $ to prefix non public channels: " "ex: $private_chan"),
+        help_text=_(
+            "Use $ to prefix non public channels: " "ex: $private_chan"),
     )
     level = models.CharField(
         max_length=20, choices=LEVELS, verbose_name=_("Authorized for")
     )
     is_active = models.BooleanField(default=True, verbose_name=_("Active"))
-    groups = models.ManyToManyField(Group, blank=True, verbose_name=_("Groups"))
+    groups = models.ManyToManyField(
+        Group, blank=True, verbose_name=_("Groups"))
     objects = ChannelManager()
 
     class Meta:
@@ -50,7 +52,7 @@ class Channel(models.Model):
         return f"{self.name} ({self.level})"
 
     def save(self, *args, **kwargs):
-        # chack channel name
+        # check the channel name
         if self.level != "public":
             if self.name.startswith("$") is False:
                 self.name = ensure_channel_is_private(self.name)
