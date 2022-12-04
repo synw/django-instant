5c76ab5b23a32114dceb11144012c7de0291e6b2
synw <synwx@protonmail.com>
2022-12-04 11:37:56 +0100

    Minor fix: remove debug info

--- instant/token.py
+++ instant/token.py
@@ -4,7 +4,6 @@ import time
 from .conf import CENTRIFUGO_HMAC_KEY
 
 def connection_token(user):
-    # CENTRIFUGO_HMAC_KEY = getattr(settings, "CENTRIFUGO_HMAC_KEY", None)    
     if CENTRIFUGO_HMAC_KEY is None:
         raise ValueError("Provide a CENTRIFUGO_HMAC_KEY in settings")
     claims = {"sub": user.get_username(), "exp": int(time.time()) + 24 * 60 * 60}
@@ -13,8 +12,6 @@ def connection_token(user):
 
 
 def channel_token(channel, user):
-    # CENTRIFUGO_HMAC_KEY = getattr(settings, "CENTRIFUGO_HMAC_KEY", None)
-    print("------------------ HMK", CENTRIFUGO_HMAC_KEY)
     if CENTRIFUGO_HMAC_KEY is None:
         raise ValueError("Provide a CENTRIFUGO_HMAC_KEY in settings")
     claims = {
